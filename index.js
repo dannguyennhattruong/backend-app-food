639dba0882720821425de2ec0697e39d65d27337
dannguyennhattruong <nhattruong2513@gmail.com>
2020-09-04 22:13:01 +0700

    user

--- index.js
+++ index.js
@@ -84,3 +84,13 @@ app.post('/signin', async (req, res) => {
         })
     }
 })
+
+app.post('/users', async (req,res) => {
+    try {
+        const users = await UserModel.find({});
+        res.send(users);
+    } catch (error) {
+        res.send(error)
+    }
+})
+
